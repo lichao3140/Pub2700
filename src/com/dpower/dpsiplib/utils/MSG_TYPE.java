b9e7c90046434c8188190573e819eca6de279c0b
lichao3140 <396229938@qq.com>
2017-06-19 13:59:25 +0800

    代码优化

---
+++ src/com/dpower/dpsiplib/utils/MSG_TYPE.java
@@ -0,0 +1,13 @@
+package com.dpower.dpsiplib.utils;
+
+public class MSG_TYPE {
+	
+	/**室内机解绑一台手机*/
+	public static final int MSG_BACK_UNBIND_PHONE_ONE = 1;
+	/**室内机解绑所有手机*/
+	public static final int MSG_BACK_UNBIND_PHONE_ALL = 2;
+	/**室内机解绑安卓手机*/
+	public static final int MSG_BACK_UNBIND_PHONE_AND = 3;
+	/**室内机解绑苹果手机*/
+	public static final int MSG_BACK_UNBIND_PHONE_IOS = 4;
+}
---
+++ src/com/dpower/util/ReceiverAction.java
@@ -0,0 +1,29 @@
+package com.dpower.util;
+
+public class ReceiverAction {
+
+	/** 室内机解绑一台手机成功发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_ONE_SUCCESS = "action.unbind.phone.one.success";
+	
+	/** 室内机解绑一台手机失败发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_ONE_FAILED = "action.unbind.phone.one.failed";
+	
+	/** 室内机解绑全部手机成功发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_ALL_SUCCESS = "action.unbind.phone.all.success";
+	
+	/** 室内机解绑全部手机失败发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_ALL_FAILED = "action.unbind.phone.all.failed";
+	
+	/** 室内机解绑安卓手机成功发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_AND_SUCCESS = "action.unbind.phone.and.success";
+	
+	/** 室内机解绑安卓手机失败发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_AND_FAILED = "action.unbind.phone.and.failed";
+	
+	/** 室内机解绑苹果手机成功发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_IOS_SUCCESS = "action.unbind.phone.ios.success";
+	
+	/** 室内机解绑苹果手机失败发送广播的Action */
+	public final static String ACTION_UNBIND_PHONE_IOS_FAILED = "action.unbind.phone.ios.failed";
+	
+}
