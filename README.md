b3d0c60b9bba5491917475e48c472096c29910b0
lichao3140 <396229938@qq.com>
2017-10-10 15:46:24 +0800

    图片

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Pub2700
-android indoor
+# InDoor
+![]{/screenshot/device-2017-10-10-153933.png "主页"}
+![]{/screenshot/device-2017-10-10-154009.png "云对讲"}
+![]{/screenshot/device-2017-10-10-154019.png "家庭安防"}
+![]{/screenshot/device-2017-10-10-154028.png "可视对讲"}
+![]{/screenshot/device-2017-10-10-154039.png "智能家居"}
+![]{/screenshot/device-2017-10-10-154049.png "设置"}
+![]{/screenshot/device-2017-10-10-154059.png "娱乐天地"}
+
---
+++ obj/local/armeabi/objs/Base64Code/Base64Code.o.d
@@ -0,0 +1,79 @@
+obj/local/armeabi/objs/Base64Code/Base64Code.o: jni/Base64Code.c \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/string.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/malloc.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/alloca.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/strings.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/stdint.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/memory.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/stdio.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/jni.h \
+ jni/com_dpower_pub_dp2700_tools_JniBase64Code.h \
+ G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/android/log.h
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/string.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/strings.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/memory.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/jni.h:
+
+jni/com_dpower_pub_dp2700_tools_JniBase64Code.h:
+
+G:/ndk-10/android-ndk-r10d/platforms/android-9/arch-arm/usr/include/android/log.h:
